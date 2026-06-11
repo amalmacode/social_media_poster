c4808fb09642363566adcf103c611f4ce307316d
amalmacode <amaldotrading@gmail.com>
2026-06-12 00:13:06 +0100

    REMOVE REDIRECT TO LOGIN PAGE MOMENTALLy

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,3 +1,5 @@
+const userModel = require('../models/userModel');
+
 function ensureGuest(req, res, next) {
   if (req.isAuthenticated()) return res.redirect('/');
   return next();
@@ -5,8 +7,27 @@ function ensureGuest(req, res, next) {
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) return next();
-  req.flash('error', 'Please log in to continue.');
-  return res.redirect('/auth/login');
+
+  // --- TEMP (TikTok API review): allow access without login so reviewers aren't
+  // redirected to /auth/login. Remove this block once API access is approved to
+  // restore the redirect below.
+  userModel.findByEmail('test@socialpost.urlyapp.com')
+    .then((reviewUser) => {
+      if (!reviewUser) {
+        req.flash('error', 'Please log in to continue.');
+        return res.redirect('/auth/login');
+      }
+      req.login(reviewUser, (err) => {
+        if (err) return next(err);
+        return next();
+      });
+    })
+    .catch(next);
+  return;
+  // --- END TEMP ---
+
+  // req.flash('error', 'Please log in to continue.');
+  // return res.redirect('/auth/login');
 }
 
 module.exports = { ensureGuest, ensureAuthenticated };
